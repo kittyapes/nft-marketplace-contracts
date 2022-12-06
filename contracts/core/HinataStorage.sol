8ce063e198fd0d921a0b64afad8d07d2e834c53a
kittyapes <kittyapes@gmail.com>
2022-12-07 01:41:48 +1000

    update ipfs hash workflow

--- contracts/core/HinataStorage.sol
+++ contracts/core/HinataStorage.sol
@@ -116,7 +116,7 @@ contract HinataStorage is
         string memory uri_
     ) public {
         require(hasRole(MINTER_ROLE, msg.sender), "Hinata: NO_MINTER_ROLE");
-        uris[id] = uri_;
+        baseURI = uri_;
         _mint(to, id, amount, data);
     }
 
