8e70fa275edaa1504641dd435b48bbf8660ff1b4
kittyapes <kittyapes@gmail.com>
2023-01-24 10:08:55 +1000

    update poc deploy

--- scripts/poc-deploy.js
+++ scripts/poc-deploy.js
@@ -2,32 +2,19 @@ const { ethers, upgrades } = require('hardhat');
 
 async function main() {
   const owner = '0xD7D4587b5524b32e24F1eE7581D543C775df27B5';
-  const MockERC20Factory = await ethers.getContractFactory('MockERC20');
-  const HinataFactory = await ethers.getContractFactory('Hinata');
-  const HinataStorageFactory = await ethers.getContractFactory('HinataStorage');
+  const beneficiary = '0xD7D4587b5524b32e24F1eE7581D543C775df27B5';
+  const factoryAddr = '0x7FeDd7Cc42E5486f2Ff73147DD9c06b80665B2A1';
   const HinataMarketV2Factory = await ethers.getContractFactory('HinataMarketV2');
 
-  const weth = await MockERC20Factory.deploy('Mock WETH', 'WETH', '1000000000000000000000000');
-  const hinata = await HinataFactory.deploy(owner);
-  const storage = await upgrades.deployProxy(
-    HinataStorageFactory,
-    [[owner], hinata.address, weth.address],
-    { initializer: 'initialize(address[],address,address)', kind: 'uups' },
-  );
-  const marketplace = await upgrades.deployProxy(
+  const marketV2 = await upgrades.deployProxy(
     HinataMarketV2Factory,
-    [storage.address, 1000, owner],
+    [[owner], factoryAddr, beneficiary, 0],
     { initializer: 'initialize', kind: 'uups' },
   );
 
-  await hinata.deployed();
-  await hinata.setStorage(storage.address);
-  await storage.deployed();
-  await marketplace.deployed();
+  await marketV2.deployed();
 
-  console.log('Hinata at:', hinata.address);
-  console.log('Storage at:', storage.address);
-  console.log('Marketplace at:', marketplace.address);
+  console.log('MarketV2 at:', marketV2.address);
 }
 
 main()
